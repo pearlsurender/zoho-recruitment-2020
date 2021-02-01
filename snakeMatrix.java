60d822b51c3186c333c3b547e9701cef7dfeb3b4
pearlsurender <78217997+pearlsurender@users.noreply.github.com>
2021-02-01 09:56:16 +0530

    Update snakeMatrix.java

--- snakeMatrix.java
+++ snakeMatrix.java
@@ -10,6 +10,20 @@ n=s.nextInt();
        for(j=1;j<=(2*n)-i;j++)  
        {
         System.out.print(j<=n-i?" ":i%2==1?++k:k--);
+          /*if(j<=n-i)            //print the spaces
+           {
+            System.out.print(" ");
+           }
+           else if(i%2==1)       //print the odd rows
+           {
+               k++;
+               System.out.print(k);
+           }
+           else                  //print the even rows
+           {
+            System.out.print(k);
+               k--;             //decrement the values
+           }*/
        }
        k+=n;                  
        System.out.println();
